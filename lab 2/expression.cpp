24ca839ff2195341dbb7caec13c6aeaa26ec52b0
Philip Robertsson <91731687+PhilipRobertsson@users.noreply.github.com>
2022-11-24 16:44:36 +0100

    Add files via upload

---
+++ lab 2/expression.cpp
@@ -0,0 +1,27 @@
+/*********************************************************
+ * Class Expression member functions implementation       *
+ * TNG033: Lab 2                                          *
+ **********************************************************/
+
+#include "expression.h"
+
+// Initialize the counter of the total number of existing Expressions
+std::size_t Expression::count_expressions = 0;
+
+// Used only for debug purposes
+// Return number of existing Expressions
+std::size_t Expression::get_count_expressions() {
+    return Expression::count_expressions;
+}
+
+/* ************************* */
+
+bool Expression::isRoot(double x) const {
+    double temp = abs(this->operator()(x));
+    return(temp < Epsilon);
+}
+
+std::ostream& operator<<(std::ostream& os, const Expression& E) {
+    E.display(os);
+    return os;
+}
